f33dd90a11b884a981f976392fc86d5e26a2be03
GolamKadirSakif <golamkadirsakif@gmail.com>
2025-06-09 03:55:01 +0600

    Update 24.Swap-Nodes-in-Pairs.py

--- 24.Swap-Nodes-in-Pairs.py
+++ 24.Swap-Nodes-in-Pairs.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
     def swapPairs(self, head):
 
@@ -17,7 +17,7 @@ class Solution(object):
             if flag==0:
                 flag=1
                 prev=current
-                prev_prev=current
+                
                 current=current.next
              
             else:
@@ -28,13 +28,19 @@ class Solution(object):
                     
                     current.next=prev
                     prev_prev.next=current
+                    prev_prev=prev
                     
-                    current=prev.next #ok
+                    if prev.next!=None:
 
+                        current=prev.next #ok
+                    else:
+
+                        current=None
                 else:
                     head=current
                     prev.next=current.next#ok
                     current.next=prev
+                    prev_prev=prev
                     
                     
                     if prev.next!=None:
@@ -47,24 +53,3 @@ class Solution(object):
 
         
         return head
-def list_to_linked_list(lst):
-    """Convert a Python list into a linked list."""
-    if not lst:
-        return None
-    head = ListNode(lst[0])
-    current = head
-    for val in lst[1:]:
-        current.next = ListNode(val)
-        current = current.next
-    return head
-
-
-lst = [1,2,3,4]
-linked_list_heads = list_to_linked_list(lst)
-
-x=Solution()
-z=x.swapPairs(linked_list_heads)
-
-print(z.val)
-print(z.next.val)
-print(z.next.next.val)
