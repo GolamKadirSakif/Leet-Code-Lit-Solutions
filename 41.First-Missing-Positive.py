efcc371859b1a15340e63c9800041d3d86d49fb3
GolamKadirSakif <golamkadirsakif@gmail.com>
2025-06-12 14:42:29 +0600

    Update 41.First-Missing-Positive.py
    Updated Version code works. But Not the most efficient version.

--- 41.First-Missing-Positive.py
+++ 41.First-Missing-Positive.py
@@ -1,38 +1,69 @@
-class Solution(object):
-    def firstMissingPositive(self, nums):
-        
 
-       
-        if len(nums)>10**5 or len(nums)<1:
-            return None
-        
-        for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                if nums[i]>nums[j]:
-                    x=nums[i]
-                    nums[i]=nums[j]
-                    nums[j]=x
-        
-       
+def firstMissingPositive(nums):
+    sort=sorted(nums)
+    l=len(sort)
 
+    
+    #Dealing with the negatives and zero
 
-        
+    if sort[l-1]<0:
+        return 1
 
-        max_int=nums[len(nums)-1]
-        
-
-        
-        val=None
-        for i in range(1, max_int+1):
-            if i in nums:
-                pass
-            else:
-                val=i
-                break
-
+    i=0
+    while sort:
+        if sort[i]<=0:
+            sort.pop(i)
             
-        if val==None:
-            return max_int+1
-
         else:
-            return val
+            break
+    
+    #Side Case -1
+    if sort==[]:
+        return 1
+
+    #Dealing With repeated numbers
+    l=len(sort)
+
+    i=0
+    j=1
+
+    while sort:
+        if l==1:
+            break
+        if sort[i]==sort[j]:
+            sort.pop(j)
+            
+        elif sort[i]!=sort[j]:
+            i+=1
+            j+=1
+        if j>(len(sort)-1):
+            break
+
+
+
+
+
+    ####
+    
+    
+
+
+    # Main Function
+    l=len(sort)#new len
+
+    for i in range(1,l+1):
+        if i == sort[i-1]:
+            pass
+        else:
+            return i
+
+    return (sort[-1]+1)
+
+
+
+
+
+
+
+
+print(firstMissingPositive([1]))
